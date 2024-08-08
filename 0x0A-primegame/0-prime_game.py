9d384d9bd71b5030c2c6ab8e4a69ee1ee0c0222c
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-08-08 19:15:03 +0000

    task

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module for Prime Game"""
+
+
+def sieve_of_eratosthenes(max_n):
+    """Returns a list of booleans where True means that index is a prime number."""
+    is_prime = [True] * (max_n + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
+    for i in range(2, int(max_n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_n + 1, i):
+                is_prime[j] = False
+    return is_prime
+
+def isWinner(x, nums):
+    if x < 1 or not nums:
+        return None
+
+    max_n = max(nums)
+    is_prime = sieve_of_eratosthenes(max_n)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        primes = sum(is_prime[:n+1])
+        if primes % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
